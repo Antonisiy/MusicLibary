b9ecfe58a14e425488f7da9107666b4b639cdeba
Meanto <meanto@yandex.ru>
2019-02-05 00:25:33 +0300

    update

--- DatabaseCRUD/DatabaseCRUD/Model/GenresModel.cs
+++ DatabaseCRUD/DatabaseCRUD/Model/GenresModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Linq;
+using System;
 
 namespace DatabaseCRUD.DataModel
 {
@@ -20,13 +22,22 @@ namespace DatabaseCRUD.DataModel
 
         public void UpdateGenre(Genre updatedGenre)
         {
-                _genreRepository.Update(updatedGenre);
+            _genreRepository.Update(updatedGenre);
         }
 
         public void CreateGenre(Genre newGenre)
         {
+            if (Genres.FirstOrDefault(u => u.Name.Equals(newGenre.Name, StringComparison.CurrentCultureIgnoreCase)) == null)
+            {
                 _genreRepository.Create(newGenre);
                 Genres.Add(newGenre);
+            }
+            else
+            {
+                MessageBox.Show("Такой жанр уже добавлен!", "Ошибка!");
+                return;
+            }
+          
         }
 
         public void DeleteGenre(Genre delGenre)
